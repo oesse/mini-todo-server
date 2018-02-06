f41913096116935513879a4ddaa999de370c8bce
Oliver Esser <oliver.esser@actano.de>
2018-02-06 17:22:04 +0100

    feat: add minimal todo list items rest api

--- src/service.js
+++ src/service.js
@@ -1,3 +1,48 @@
+import express, { Router } from 'express';
+import bodyParser from 'body-parser';
+import { CREATED } from 'http-status-codes';
+import _debug from 'debug';
+
+const debug = _debug('request');
+
+const items = [];
+
+const api = Router();
+
+api.get('/items', (req, res) => {
+  debug('get items');
+  res.json(items);
+});
+
+api.put('/items', (req, res) => {
+  const { name, status } = req.body;
+  const newItem = { name, status };
+
+  const itemIndex = items.findIndex(i => i.name === name);
+  if (itemIndex === -1) {
+    debug('put new item', newItem);
+    items.push(newItem);
+    res.status(CREATED);
+  } else {
+    debug('put existing item', newItem);
+    items[itemIndex] = newItem;
+  }
+
+  res.json(newItem);
+});
+
 export default () => ({
-  start() { console.log('Service started'); },
+  start() {
+    const app = express();
+
+    app.use(bodyParser.json());
+
+    app.get('/', (req, res) => {
+      res.send('Welcome to doing things\n');
+    });
+
+    app.use('/api', api);
+
+    app.listen(8080);
+  },
 });
